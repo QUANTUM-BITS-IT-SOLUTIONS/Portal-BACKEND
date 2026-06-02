18e094fca4d8742c9772b2ab325d4b0a7d425938
Rockstarrrr20 <arnavrj2001@gmail.com>
2026-06-03 02:06:47 +0530

    trail 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient(
-    { log: ["query", "warn", "error"], }
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["warn", "error"],
+    });
 
-);
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
